a3ec0df937e0c885684d7bdb4fadd5ecafa6ed1c
PeterParker <51321351+zhuchangyi@users.noreply.github.com>
2023-12-18 09:37:34 +0800

    Update gemini2tg.sh

--- gemini2tg.sh
+++ gemini2tg.sh
@@ -1,12 +1,28 @@
 #!/bin/bash
-sudo apt-get update -y
-mkdir gemini
-cd gemini/
 sudo add-apt-repository -y ppa:deadsnakes/ppa
-sudo apt-get install python3.9 -y
-sudo apt install python3.9-venv -y
-python3.9 -m venv gemini2tg
+sudo apt-get update -y
+sudo apt-get install python3 -y
+current_python_version=$(python3 -c 'import platform; print(platform.python_version())')
+required_python_version="3.9"
+mkdir -p gemini
+cd gemini/
+
+if [[ $(printf '%s\n' "$required_python_version" "$current_python_version" | sort -V | head -n1) != "$required_python_version" ]]; then
+    echo "current_python_version ($current_python_version) less than ($required_python_version)install python3.9"
+    sudo apt-get install python3.9 -y
+    sudo apt install python3.9-venv -y
+    python3.9 -m venv gemini2tg
+else
+    echo "current_python_version ($current_python_version)>3.9"
+    sudo apt install python3-venv -y
+    python3 -m venv gemini2tg
+fi
+
+
+
 source gemini2tg/bin/activate
+
+
 pip install python-telegram-bot==13.13
 pip install Pillow
 pip install -q -U google-generativeai
@@ -37,3 +53,5 @@ else
 fi
 chmod +x script.py
 nohup python3.9 script.py &
+nohup python3 script.py &
+echo "Gemini2tg is running in the background."
